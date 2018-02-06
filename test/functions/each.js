2701155ef1509dd6f555cf3721147c4cbbb5b0a7
Ernesto Freyre <ernesto@airtime.com>
2018-02-05 22:54:04 -0800

    improved each for reusability

--- test/functions/each.js
+++ test/functions/each.js
@@ -17,3 +17,18 @@ test('runs all tasks in serie', t => {
       t.true(duration < 1200)
     })
 })
+
+
+test('starting with each good for reusable steps', t => {
+  const step = each(
+    delayedAsync(100, { name: 'John' }),
+    delayedAsync(100, { last: 'Doe' }),
+  )
+
+  t.truthy(step)
+
+  return step()
+    .then(ctx => {
+      t.deepEqual(ctx, { name: 'John', last: 'Doe' })
+    })
+})
